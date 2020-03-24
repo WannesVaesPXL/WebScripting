ab62d8f609c5c820d94eec8e24e3578f42c64fb5
WannesVaesPXL <Wannes.Vaes@student.pxl.be>
2020-03-24 21:38:23 +0100

    Exercise 2 done

--- voorbeeldexamen/uittezenden_webscripting/oef2/js/fetchShips.js
+++ voorbeeldexamen/uittezenden_webscripting/oef2/js/fetchShips.js
@@ -1,10 +1,12 @@
 // naam: Wannes Vaes
 
 window.addEventListener("load", handleWindowload);
+document.getElementById("get_ships").addEventListener("click", handleGetShips);
 
 function handleWindowload() {
     let url = 'http://localhost:3000/country/';
     let dropdown = document.getElementById("div_select").appendChild(document.createElement("select"));
+    dropdown.length = 0;
     dropdown.setAttribute("id", "country_select");
 
     let defaultOption = document.createElement('option');
@@ -42,6 +44,60 @@ function handleWindowload() {
 }
 
 function handleGetShips() {
+    var e = document.getElementById("country_select");
+    var selectValue = e.options[e.selectedIndex].value;
     let url = 'http://localhost:3000/ship/';
-    
+    let output = document.getElementById("div_output");
+    let list = document.createElement("ul");
+    makeElementEmpty(output);
+    fetch(`${url}?country_id=${selectValue}`)
+        .then((response) =>{
+            if (response.status==200) {
+                return response.json();
+            } else {
+                throw `error with status ${response.status}`;
+            }
+        })
+        .then((ships) => {
+            var fastestShip  = 0;
+            for (let ship of ships) {
+                if (ship.speed > fastestShip){
+                    fastestShip = ship.speed;
+                }
+            }
+            for (let ship of ships) {
+                li = document.createElement("li");
+                if (ship.speed == fastestShip){
+                    li.setAttribute("style", "color:red");
+                }
+                listItemText = document.createTextNode(`${ship.id}, ${ship.name}`);
+                li.appendChild(listItemText)
+                list.appendChild(li);
+            }
+            output.appendChild(list);
+        })
+        .catch( (error) => {
+            output.appendChild(document.createTextNode(error));
+        });
+
+}
+
+function makeElementEmpty(element) {
+    while (element.hasChildNodes()) {
+        element.removeChild(element.firstChild);
+    }
+}
+
+function makeTable(matrix) {
+    let table = document.createElement("table");
+    for (let i = 0; i < matrix.length; i++) {
+        let tr = document.createElement("tr");
+        for (let j = 0; j < matrix[i].length; j++) {
+            let td = document.createElement("td");
+            td.appendChild(document.createTextNode(matrix[i][j]));
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
+    return table;
 }
