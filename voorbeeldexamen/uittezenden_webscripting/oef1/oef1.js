b503af77db39ff52ec2ecbf3892bbdb0cb91f660
WannesVaesPXL <Wannes.Vaes@student.pxl.be>
2020-03-24 16:05:14 +0100

    oef1 done (class Loonwerker & class Manager added)

--- voorbeeldexamen/uittezenden_webscripting/oef1/oef1.js
+++ voorbeeldexamen/uittezenden_webscripting/oef1/oef1.js
@@ -6,13 +6,13 @@ class Persoon{
        this._id=id;
        }
        else{
-            throw "Dit is geen getal groter dan 0.";
+            throw `[${id}]: Deze id is geen getal groter dan 0.`;
        }
        if ((typeof name)=='string'){
            this._name=name;
        }
        else{
-           throw "Dit is geen string."
+           throw `[${name}]: Dit is geen string.`;
        }
     }
     toString(){
@@ -20,21 +20,72 @@ class Persoon{
     }
 }
 
+class Loonwerker extends Persoon{
+    loon;
+    constructor(id, name, loonPerUur, aantalUrenGewerkt){
+        super(id, name);
+        if (loonPerUur>0 && Number.isInteger(loonPerUur)){
+            this._loonPerUur=loonPerUur;
+        }
+        else{
+            throw `[${loonPerUur}]: Dit uurloon is geen getal groter dan 0.`;
+        }
+        if (aantalUrenGewerkt>0 && Number.isInteger(aantalUrenGewerkt)){
+            this._aantalUrenGewerkt=aantalUrenGewerkt;
+        }
+        else {
+            throw `[${aantalUrenGewerkt}]: Het aantal gewerkte uren is geen getal groter dan 0.`;
+        }
+    }
+    berekenLoon(){
+        this.loon = this._aantalUrenGewerkt * this._loonPerUur
+        return this.loon;
+    }
 
+    toString() {
+        return super.toString() + ` = ${this.berekenLoon()}`;
+    }
+}
+
+let loonWerkers = [];
+class Manager extends Persoon{
+    constructor(id, name){
+        super(id, name);
+    }
+    voegLoonWerkerToe(loonwerker) {
+        if (loonwerker instanceof Loonwerker){
+            loonWerkers.push(loonwerker);
+        }
+        else{
+            throw `[${loonwerker}]: Dit is geen loonwerker.`;
+        }
+    }
+    berekenLoon(){
+        let lonen = 0;
+        for ( let i = 0; i < loonWerkers.length; i++){
+            lonen += loonWerkers[i].loon;
+        }
+        return Math.round(lonen *0.2);
+    }
+    toString(){
+        return super.toString() + ` = ${this.berekenLoon()}`;
+    }
+
+}
 
 
 let persoon = new Persoon(1,"mieke");
-// let manager=new Manager(2,"jan");
-// let werker1=new Loonwerker(3,"tim",11,13);
-// let werker2=new Loonwerker(4,"sofie",2,50);
-// manager.voegLoonWerkerToe(werker1);
-// manager.voegLoonWerkerToe(werker2);
+let manager=new Manager(2,"jan");
+let werker1=new Loonwerker(3,"tim",11,13);
+let werker2=new Loonwerker(4,"sofie",2,50);
+manager.voegLoonWerkerToe(werker1);
+manager.voegLoonWerkerToe(werker2);
 console.log(persoon.toString());
 // [1] mieke 
-// console.log(werker1.toString());
+console.log(werker1.toString());
 // // [3] tim = 143
-// console.log(werker2.toString());
+console.log(werker2.toString());
 // // [4] sofie = 100
-// console.log(manager.toString());
+console.log(manager.toString());
 // // [2] jan = 49
 
